3cdb73b5cae02b6729f42f40765f30bff09e71cf
D2J3D <120342275+D2J3D@users.noreply.github.com>
2023-06-09 14:21:45 +0300

    Update Main.java

--- java/org/example/Main.java
+++ java/org/example/Main.java
@@ -5,23 +5,24 @@ import java.sql.SQLException;
 import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) throws SQLException {
         DBConnector dbConnector = new DBConnector();
-        //dbConnector.addFilm(1, "Пила 2", 160, "ru", 15, "thriller", "", "USA", "USA", 4);
+        // Массивы данных, необходимых для добавления строки в базу данных Films и Directors
         String[] directors = new String[] { "Кристофер Нолан", "Мартин Скорсезе", "Гай Ричи", "Джеймс Кэмерон", "Дэвид Финчер", "Тим Бёртон", "Стэнли Кубрик", "Люк Бессон", "Андрей Тарковский", "Хаяо Миядзаки", "Роберт Земекис", "Питер Джексон"};
-        String[] countries = new String[] {"Россия","США", "Великобритания", "Франция", "Италия", "Япония"};
+        String[] countries = new String[] {"Россия","США", "Великобритания", "Франция", "Италия", "Япония"}; // Список стран для фильмов
         String[] filmNames = new String[] { "Тайна Коко", "Властелин Колец", "Интерстеллар",  "Бойцовский клуб", "Криминальное чтиво", "Иван Васильевич меняет профессию", "Назад в будущее", "Король Лев", "1+1", "Приключения Шерлока Холмса", "Шрек", "Москва слезам не верит", "Достучаться до небес", "Остров Проклятых", "Джентельмиены удачи", "Гарри Поттер и узник Азкабана", "Темный рыцарь", "Операция Ы", "Девчата", "Ходячий замок", "Терминатор", "Гарри Поттер и философский камень", "Пираты Карибского моря", "Джентельмены", "Гладиатор", "Поймая меня, если сможешь", "Игра на понижение", "Начало", "Титаник", "Большой куш", "Клаус", "Один дома", "Один дома 2", "Один дома 3", "Гарри Поттер и Тайная комната", "Гарри Поттер и Дары Смерти", "Бриллиантовая рука", "Зеленаяя книга", "Остров сокровищ", "Хатико", "Карты, деньги, 2 ствола", "Как приручить дракона", "Джанго", "Прислуга", "Шрэк 2", "Леон", "Пианист", "Песнь моря", "Пираты Карибского моря 2", "Рататуй",  "Крестный отец", "Укрощение строптивого", "Пеле", "Зверополис", "Запах женщины", "Хоббит", "Корпорация монстров", "Матрица", "Матрица 2", "Матрица 3", "Одержимость", "Мулан", "Гарри Поттер и Кубок огня", "Звездные войны. Эпизод 3", "Мадагаскар", "Крестный отец 2"};
         String[] filmGenres = new String[] {"Комедия", "Вестерн", "Фильм-катастрофа", "Документальный", "Фильм-ужасов"};
-//        for (int i = 0; i < directors.length; i++){
-//            String[] directorName = directors[i].split(" ");
-//            int directedFilmsAmount = (int) (Math.random() * 20);
-//            int birthYear = 1946 +  (int) (Math.random() * 20);
-//            int rnd = new Random().nextInt(countries.length);
-//            String mortherCountry = countries[rnd];
-//            System.out.println(directorName[0] + " " + directorName[1] + " " + directedFilmsAmount + " " + birthYear + " " + mortherCountry);
-//            dbConnector.addDirector(directorName, directedFilmsAmount, birthYear, mortherCountry);
-//        }
+        // Скрипт для заполнения таблицы Directors
+        for (int i = 0; i < directors.length; i++){
+            String[] directorName = directors[i].split(" ");
+            int directedFilmsAmount = (int) (Math.random() * 20);
+            int birthYear = 1946 +  (int) (Math.random() * 20);
+            int rnd = new Random().nextInt(countries.length);
+            String mortherCountry = countries[rnd];
+            System.out.println(directorName[0] + " " + directorName[1] + " " + directedFilmsAmount + " " + birthYear + " " + mortherCountry);
+            dbConnector.addDirector(directorName, directedFilmsAmount, birthYear, mortherCountry);
+        }
+        // Скрипт для заполнения таблицы Films
         int userCounter = 1;
         int directorCounter = 4;
         for (int i = 0; i < filmNames.length; i++){
@@ -53,6 +54,5 @@ public class Main {
             }
         }
         System.out.println(filmNames.length);
-
     }
 }
